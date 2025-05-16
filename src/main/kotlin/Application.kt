aec8e44be12bd299fcbed70fff12edbed11cb25b
platonso <plat100916@gmail.com>
2025-05-16 18:03:32 +0300

    initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package com.platonso
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
